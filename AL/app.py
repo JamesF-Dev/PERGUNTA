e8a4f8a29e0019f858f2b17a724a579abed6a985
James <jemesfranklin6@gmail.com>
2026-09-21 16:58:55 -0300

    corrige dependencias do Flask

--- AL/app.py
+++ AL/app.py
@@ -104,7 +104,7 @@ pagina = """
 
         function clicouSim() {
             document.getElementById("pergunta").innerHTML =
-                "Eu sabia sua covarde! 😭❤️<br><br>Agora me perdoa? 🥺";
+            "Eu sabia sua covarde! 😭<br><br>Agora me perdoa? 🥺";
         }
     </script>
 
